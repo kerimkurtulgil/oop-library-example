10c0e44e451affe1d11aeda6a64d00ce03201fa3
kerimkurtulgil <122718308+kerimkurtulgil@users.noreply.github.com>
2025-01-10 13:26:34 +0000

    Create empty classes

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello from the Library project')
+class Library:
+    pass
+
+class Book:
+    pass
+
+#Creating instance for Book class
+book1=Book();
+book2=Book();
+
+
+#Creating instance for library class
+library1=Library();
+library2=Library();
+
+
+git add main.py
+git commit -m 'Create empty classes'
+git push origin main
